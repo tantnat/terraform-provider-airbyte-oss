9745b670b8f48dfeacd23070a8745f29ea48b2f5
Antoine Balliet <antoine.balliet@gorgias.com>
2023-07-29 15:25:03 +0200

    initial commit

---
+++ RELEASES.md
@@ -0,0 +1,7 @@
+
+
+## 2023-07-29 13:11:18
+### Changes
+Based on:
+- OpenAPI Doc 1.0.0 
+- Speakeasy CLI 1.65.2 (2.75.1) https://github.com/speakeasy-api/speakeasy
---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    airbyte = {
+      source  = "aballiet/airbyte"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "airbyte" {
+  # Configuration options
+}
